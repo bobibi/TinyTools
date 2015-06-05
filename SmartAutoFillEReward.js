0d5ce9ccb4cf352504a3be511322ad091ac3f16d
bobibi <bobibi@users.noreply.github.com>
2015-06-05 15:47:23 -0700

    Update SmartAutoFillEReward.js

--- SmartAutoFillEReward.js
+++ SmartAutoFillEReward.js
@@ -1,3 +1,25 @@
+/**
+ * SmartAutoFillEReward
+ * 
+ * This is used to deal with the boring and endless survey. You will never need to repeat clicking
+ * your mouse to check the radios and checkbox. BE CAREFUL: this tool has no intelligence to understand
+ * what's the question is. The only thing it does is randomly check a radio and always check a checkbox.
+ * 
+ * Only use it if 1) you don't need to understand the survey, 2) you don't care what your answers will be,
+ * and 3) the outcome of the survey will not bring any damages.
+ * 
+ * How to install and use it:
+ * 1) You must have a webserver that you can access it from public domain.
+ * 2) Put this file in the web folder of the server.
+ * 3) Create a browser bookmark in the computer where you want to use this tool, and copy and paste following
+ *    to the URL box: 
+ *    javascript:document.getElementsByTagName('body')[0].appendChild(document.createElement('script')).setAttribute('src','<<Your web host address>>/SmartEReward.js');
+ * 4) Click the bookmark when you need to use it.
+ * 
+ * Enjoy!
+ * 
+ **/
+
 (function(){
 	console.clear();
 	//console.info("SmartEReward Start");
@@ -11,9 +33,9 @@
 		//console.info(inputList[i])
 		if(inputList[i].getAttribute("type").toLowerCase() == "radio" && !ArrayContains(radioNameList, inputList[i].getAttribute("name"))) {
 			radioNameList.push(inputList[i].getAttribute("name"));
-        } else if(inputList[i].getAttribute("type").toLowerCase() == "checkbox") {
-            inputList[i].setAttribute("checked", "checked");
-        }
+        	} else if(inputList[i].getAttribute("type").toLowerCase() == "checkbox") {
+            		inputList[i].setAttribute("checked", "checked");
+        	}
 	}
 	
 	for(i=0;i<radioNameList.length;i++){
@@ -35,7 +57,9 @@
 			//console.info(inputList[i])
 			if(inputList[i].getAttribute("type").toLowerCase() == "radio" && !ArrayContains(radioNameList, inputList[i].getAttribute("name"))) {
 				radioNameList.push(inputList[i].getAttribute("name"));
-			}
+			} else if(inputList[i].getAttribute("type").toLowerCase() == "checkbox") {
+            			inputList[i].setAttribute("checked", "checked");
+        		}
 		}
 		
 		for(i=0;i<radioNameList.length;i++){
