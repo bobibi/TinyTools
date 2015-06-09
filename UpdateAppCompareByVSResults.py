5900f51f69cde153cac24b34a36963e6c5333ae1
bobibi <bobibi@users.noreply.github.com>
2015-06-08 18:21:45 -0700

    Update UpdateAppCompareByVSResults.py

--- UpdateAppCompareByVSResults.py
+++ UpdateAppCompareByVSResults.py
@@ -10,6 +10,7 @@ from sqlalchemy.dialects.mysql import TIMESTAMP
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, exc
+from sqlalchemy import and_
 
 engine = create_engine("mysql://VisualThreats:@localhost/VisualThreats?charset=utf8&use_unicode=1", echo=False)
 Base = declarative_base()
