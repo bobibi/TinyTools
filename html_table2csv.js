0e28d7f91251827d43b359ef76341a99e8aa0082
bobibi <bobibi@users.noreply.github.com>
2016-02-12 14:59:37 -0800

    Update html_table2csv.js

--- html_table2csv.js
+++ html_table2csv.js
@@ -1,1 +1,16 @@
+document.getElementsByTagName('body')[0].appendChild(document.createElement('script')).setAttribute('src','http://code.jquery.com/jquery-1.10.2.js');
 
+$('table').dblclick(function(event){
+  //console.info($(event.target).parents('table'))
+  if (confirm("Download this table as CSV?") != true) return;
+  var str = '';
+  $(event.target).parents('table').find('tr').each(function(i, r){
+    var d = '';
+    $(r).find('td').each(function(j, c){
+      str += d+$(c).text();
+      d = ','
+    })
+    str += '\n';
+  })
+  console.info(str);
+})
